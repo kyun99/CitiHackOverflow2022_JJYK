7910b708064122de99964981b14f6f7031a649c2
Yunus <yunusali150201@gmail.com>
2022-08-09 14:53:30 +0800

    Initial React Structure

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Fragment } from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import BrowseCompanies from "./components/BrowseCompanies";
+import CompanyDetails from "./components/CompanyDetails";
+import Navbar from "./components/Navbar";
+
+function App() {
+   return (
+      <div className="App">
+         <Fragment>
+            <Router>
+               <Navbar />
+               <Routes>
+                  <Route exact path="" element={<Home />} />
+                  <Route exact path="about" element={<About />} />
+                  <Route exact path="companies" element={<BrowseCompanies />} />
+                  <Route exact path="details" element={<CompanyDetails />} />
+               </Routes>
+            </Router>
+         </Fragment>
+      </div>
+   );
+}
+
+export default App;
---
+++ client/src/components/BrowseCompanies.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BrowseCompanies = () => {
+   return (
+      <div>
+         <h1>Browse Companies</h1>
+      </div>
+   );
+};
+
+export default BrowseCompanies;
