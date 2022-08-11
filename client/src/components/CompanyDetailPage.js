5e72f80a2872428109a35bf2c31a30266708a197
justinhoejj <hoejj05@gmail.com>
2022-08-12 00:05:01 +0800

    unhide edit button

--- client/src/components/CompanyDetailPage.js
+++ client/src/components/CompanyDetailPage.js
@@ -3,20 +3,48 @@ import React, { Fragment, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom"
 import DetailImage from "./DetailImage";
 import DetailText from './DetailText';
+import axios from "axios";
+
 
 const CompanyDetailPage = (props) => {
-   const companyName = "Place holder name"
    const location = useLocation()
    const params = new URLSearchParams(location.search)
    const bloombergCode =  params.get('code');
-   const profile = props.profile
    const editLink = `/banker/edit?code=${bloombergCode}`
+
+  const [companyName, setName] = useState("Yangzijiang Shipbuilding Holdings Ltd")
+  const [industry, setIndustry] = useState("Logistics")
+  const [profile, setDescription] = useState("Yangzijiang is one of the largest, most efficient, and profitable shipbuilders in China. It has moved up the value chain to produce ultra-large containerships and very large bulk carriers, as well as LNG vessels.")
+  const [marketcap, setMarketcap] = useState("1.4B")
+  const [recommendation, setRecommendation] = useState("Buy")
+  const [targetprice, setTargetPrice] = useState("1.14")
 
    const [esgInitiatives, setEsgInitiatives] = useState([
     {id: 0, title: "Carbon emissions reduced by 7.3% for 1H2022", resource:'somelink', upVote: 17, downVote: 2},
     {id: 1, title: "Workplace diversity increased by 40% for 2022", resource:'somelink', upVote: 38, downVote: 130},
     {id: 2, title: "MOU signed to accelerate switch to electric vehicles by 2030", resource:'somelink', upVote: 204, downVote:43},
    ])
+
+
+   useEffect(() => {
+    axios.get(`http://127.0.0.1:8000/moneyplant/company/${bloombergCode}`)
+      .then(resp => {
+        console.log(resp.status)
+        console.log(resp.data)
+        const data = resp.data
+
+        setDescription(prev => (data.description))
+        setIndustry(prev => data.industry)
+        setMarketcap(prev => data.market_cap)
+        setName(prev => data.name)
+        setRecommendation(prev => data.recommendation)
+        setTargetPrice(prev => data.target_price)
+
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
 
    const upVote = (id) => {
       setEsgInitiatives(prevState => {
@@ -52,7 +80,7 @@ const CompanyDetailPage = (props) => {
             <h1 className="name">{companyName}</h1> 
             </Grid>
             <Grid item >
-            <div style={{ position:"relative", left:"-375px"}}>
+            <div style={{ position:"relative", left:"-180%"}}>
             { props.isBanker && <Button variant='secondary'>
               <a href={editLink} >Edit Analysis</a>
               </Button>}
